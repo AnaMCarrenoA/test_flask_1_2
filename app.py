fc42dba3a57f6b7d014d530edff086aa986ced19
Andres Medina <125099062+gatosphynx@users.noreply.github.com>
2023-08-10 23:39:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return jsonify({"payload": "welcome to my project"})
 
+@app.route("/put", methods=["PUT"])
+def delete():
+    return jsonify ({"payload":"put successfully"})
+
 if __name__ == '__main__':
     app.run(debug=True)
